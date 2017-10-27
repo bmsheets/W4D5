13a99e4bf20acab36b0641360882f01dcf3932eb
Brandt <brandtsheets@gmail.com>
2017-10-27 16:59:26 -0700

    Modify band controller and views, add nested routing

--- music_app/config/routes.rb
+++ music_app/config/routes.rb
@@ -3,7 +3,13 @@ Rails.application.routes.draw do
   resources :users, only: [:new, :create, :show]
   resource :session, only: [:new, :create, :destroy]
 
-  resources :bands
-  resources :albums, except: [:index]
+  resources :bands do
+    resources :albums, only: [:new]
+  end
+
+  resources :albums, except: [:index] do
+    resources :tracks, only: [:new]
+  end
+  
   resources :tracks, except: [:index]
 end
